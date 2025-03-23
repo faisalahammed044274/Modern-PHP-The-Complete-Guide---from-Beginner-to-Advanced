c04e610d2956b1f14024ab2c8bb279bc9092eb1a
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-23 22:25:55 +0600

    errors in PHP

---
+++ 06.errors/error.php
@@ -0,0 +1,17 @@
+<?php 
+echo "okay";
+//parse error
+//Warning
+//Fatal Error
+
+$pageTitle ="A Restaurent Website"; 
+
+echo $pagetitle;
+
+include'file.php';
+
+pageinfo();
+
+echo "Complete";
+
+?>
